537759ca1ae3b6f22c5ea5373320123efe029e6b
Gustav Hogbring <gustav.hogbring@proton.me>
2025-07-06 19:07:56 +0200

    icons in bar

--- config.def.h
+++ config.def.h
@@ -131,7 +131,7 @@ static char *colors[][ColCount] = {
  */
 static char *tagicons[][NUMTAGS] =
 {
-	[DEFAULT_TAGS]        = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+	[DEFAULT_TAGS]        = { " ", " ", " ", " ", " ", " ", " ", " ", " " },
 	[ALTERNATIVE_TAGS]    = { "A", "B", "C", "D", "E", "F", "G", "H", "I" },
 	[ALT_TAGS_DECORATION] = { "<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>", "<8>", "<9>" },
 };
